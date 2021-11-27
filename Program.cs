a8446e74394a77847634985bd21ea77a6758ea01
cecelia-tech <79083749+cecelia-tech@users.noreply.github.com>
2021-11-27 23:09:16 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CS_Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first integer: ");
+            string firstNumber = Console.ReadLine();
+            Console.Write("Enter second integer: ");
+            string secondNumber = Console.ReadLine();
+            Console.WriteLine(firstNumber, secondNumber);
+        }
+    }
+}
